aa903ddd393a4769bb2b5640923ac73a2bb04c2c
Jaroslaw Kisiolek <kisiolek.jaroslaw@gmail.com>
2018-01-27 03:20:45 +0100

    Add wrapper for looping censoring sound

---
+++ Assets/scripts/LoopingSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingSound : MonoBehaviour {
+
+	AudioSource audio;
+	public AudioClip sndStart;
+	public AudioClip sndRepeat;
+	public AudioClip sndEnd;
+	bool startPlayed = false;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("space")) {
+			if (!startPlayed) {
+				audio.clip = sndStart;
+				audio.Play();
+				startPlayed = true;
+			}
+			if (!audio.isPlaying) {
+				audio.clip = sndRepeat;
+				audio.Play ();
+			}
+		} 
+
+		if (Input.GetKeyUp("space")) {
+			audio.PlayOneShot(sndEnd);
+			startPlayed = false;
+		}
+	}
+}
