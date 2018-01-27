4ed82eb98080792131a1feb45fa1125c5e761d8f
bart <wojtowicz.bartlomiej@gmail.com>
2018-01-27 01:42:00 +0100

    channel

---
+++ Assets/Channel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Channel
+{
+    private static string TAG = "[Channel] ";
+
+    string text;
+    string[] words;
+
+    public Channel(string text)
+    {
+        string[] splitters = { @"\s+" };
+        words = text.Split(splitters, System.StringSplitOptions.RemoveEmptyEntries);
+        Debug.Log(TAG + "word count = " + words.Length);
+    }
+}
--- Assets/Tv.cs
+++ Assets/Tv.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Tv
 {
+    public static string LOREM = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id orci pharetra ex varius faucibus. Sed vulputate sollicitudin diam a fringilla. Duis dolor magna, gravida non eros sit amet, cursus eleifend ante. Donec a elit ac arcu finibus rhoncus eget id diam. Praesent lorem mauris, tempor sit amet venenatis at, mattis vel nisi. Duis vel pharetra nunc, vitae facilisis urna. Integer non aliquet erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget tellus ipsum. Aliquam id justo sed lacus gravida tristique. Integer blandit leo eget ipsum imperdiet varius. In tincidunt arcu mauris, nec lacinia augue faucibus ut.
+
+Maecenas nec dolor in arcu dictum lacinia id in enim. Maecenas venenatis augue vel bibendum eleifend. Nulla tempor neque quis arcu convallis, id ornare urna euismod. Nullam aliquet interdum aliquet. Aenean et convallis leo. Vivamus interdum lobortis nunc, vel fermentum turpis cursus eu. Morbi blandit faucibus nunc, id facilisis nisi semper a. Nulla nisi sem, rutrum in ultrices at, venenatis at purus. Donec commodo felis arcu, et accumsan purus posuere et. Vestibulum non malesuada arcu. Aliquam convallis orci et odio lacinia, sit amet imperdiet orci lobortis. Nulla ac purus aliquet, dignissim diam eget, scelerisque sem. Vivamus ornare tellus sed arcu iaculis ornare. Aenean eu finibus augue. Pellentesque tincidunt sem vitae metus tincidunt, eget sodales odio posuere. Praesent eleifend vitae ante quis pharetra.
+
+Donec at erat massa. Sed imperdiet, elit eget condimentum maximus, magna justo faucibus ante, dignissim elementum augue enim ac purus. Donec mattis et nisl ac finibus. Mauris vel hendrerit risus. In lobortis pulvinar nisi, sed elementum nisi molestie vel. Maecenas arcu odio, finibus pretium tempor quis, suscipit in eros. Pellentesque euismod mattis viverra. In maximus tincidunt velit. Sed sit amet tellus id quam tincidunt suscipit sit amet eu felis. In vel mi finibus, lacinia ligula vitae, placerat felis.
+
+Pellentesque ut orci quam. Sed malesuada diam sed odio lacinia, sit amet tempor nulla tincidunt. Vivamus condimentum congue nisi in venenatis. Sed volutpat, quam vitae tempor vehicula, libero dui suscipit sapien, luctus molestie mauris lectus a magna. In molestie viverra justo, eget consectetur tellus sagittis id. Duis rutrum elit vitae nulla feugiat varius. Curabitur mollis elementum nibh et hendrerit. Donec in urna ex. Aliquam efficitur neque lobortis lorem scelerisque, ac condimentum libero molestie. Quisque eros enim, egestas et accumsan nec, sollicitudin nec urna. Pellentesque a augue nisl. Pellentesque eu est elit. Phasellus vitae lacus sem. Etiam laoreet tincidunt quam. Nulla facilisis vel justo eu sollicitudin. Nulla ac neque purus.";
+
     public Tv()
     {
 
