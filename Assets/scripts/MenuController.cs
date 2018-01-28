1c344496a1c23a2a99854e43f658e7fc4b602991
Jaroslaw Kisiolek <kisiolek.jaroslaw@gmail.com>
2018-01-28 14:40:08 +0100

    Add menu scene

--- Assets/scripts/MenuController.cs
+++ Assets/scripts/MenuController.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MenuController : MonoBehaviour
 {
+	public Animator animator;
+
+	AudioSource audioPlayer;
+	public AudioClip sndStart;
+
+	void Start () {
+		audioPlayer = GetComponent<AudioSource>();
+	}
+
 	public void onPlayClicked()
 	{
-		Debug.Log ("aaaa");
+		animator.SetTrigger ("pressed");
+		audioPlayer.PlayOneShot (sndStart);
 		SceneManager.LoadScene("game_scene");
+	}
+
+	public void OnPointerEnter()
+	{	
+		animator.SetTrigger ("hover");
+		Debug.Log("The cursor entered the selectable UI element.");
+
+	}
+
+	public void OnPointerLeave()
+	{	
+		animator.SetTrigger ("idle");
+		Debug.Log("The cursor entered the selectable UI element.");
+
 	}
 }
